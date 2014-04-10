3935c71118ddf6028d6bca21fab05ffb6d2f2d55
Peter Havens <Peter.Havens@pinnacol.com>
2014-04-10 13:25:10 -0600

    Use Clojure 1.6.0.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject foreclojure "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Solutions for www.4clojure.com problems."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot foreclojure.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
