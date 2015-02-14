063372a696ee34fd7d98f81a5bd1472898f019aa
Peter Havens <peter.havens@gmail.com>
2015-02-14 10:57:30 -0700

    Fix formatting.

--- src/foreclojure/pairwise_disjoint_sets.clj
+++ src/foreclojure/pairwise_disjoint_sets.clj
@@ -23,45 +23,45 @@
    true)
 
 (= (pds #{#{:a :b :c :d :e}
-         #{:a :b :c :d}
-         #{:a :b :c}
-         #{:a :b}
-         #{:a}})
+          #{:a :b :c :d}
+          #{:a :b :c}
+          #{:a :b}
+          #{:a}})
    false)
 
 (= (pds #{#{[1 2 3] [4 5]}
-         #{[1 2] [3 4 5]}
-         #{[1] [2] 3 4 5}
-         #{1 2 [3 4] [5]}})
+          #{[1 2] [3 4 5]}
+          #{[1] [2] 3 4 5}
+          #{1 2 [3 4] [5]}})
    true)
 
 (= (pds #{#{'a 'b}
-         #{'c 'd 'e}
-         #{'f 'g 'h 'i}
-         #{''a ''c ''f}})
+          #{'c 'd 'e}
+          #{'f 'g 'h 'i}
+          #{''a ''c ''f}})
    true)
 
 (= (pds #{#{'(:x :y :z) '(:x :y) '(:z) '()}
-         #{#{:x :y :z} #{:x :y} #{:z} #{}}
-         #{'[:x :y :z] [:x :y] [:z] [] {}}})
+          #{#{:x :y :z} #{:x :y} #{:z} #{}}
+          #{'[:x :y :z] [:x :y] [:z] [] {}}})
    false)
 
 (= (pds #{#{(= "true") false}
-         #{:yes :no}
-         #{(class 1) 0}
-         #{(symbol "true") 'false}
-         #{(keyword "yes") ::no}
-         #{(class '1) (int \0)}})
+          #{:yes :no}
+          #{(class 1) 0}
+          #{(symbol "true") 'false}
+          #{(keyword "yes") ::no}
+          #{(class '1) (int \0)}})
    false)
 
 (= (pds #{#{distinct?}
-         #{#(-> %) #(-> %)}
-         #{#(-> %) #(-> %) #(-> %)}
-         #{#(-> %) #(-> %) #(-> %)}})
+          #{#(-> %) #(-> %)}
+          #{#(-> %) #(-> %) #(-> %)}
+          #{#(-> %) #(-> %) #(-> %)}})
    true)
 
-(= (pds #{#{(#(-> *)) + (quote mapcat) #_ nil}
-         #{'+ '* mapcat (comment mapcat)}
-         #{(do) set contains? nil?}
-         #{, , , #_, , empty?}})
+(= (pds #{#{(#(-> *)) + (quote mapcat) #_nil}
+          #{'+ '* mapcat (comment mapcat)}
+          #{(do) set contains? nil?}
+          #{#_empty?}})
    false)
